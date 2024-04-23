3d99f1456e4a5d4bc7a53239f8c4815a66ea1bbc
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-04-23 15:47:48 -0400

    testing livedata

---
+++ app/src/test/java/com/example/artbooktest/MainCoroutineRule.kt
@@ -0,0 +1,30 @@
+package com.example.artbooktest
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+//makes sure that it can be run in a main thread in the test folder as well. Wouldn't be needed in the androidtest folder
+@ExperimentalCoroutinesApi
+class MainCoroutineRule (
+    private val dispatcher: CoroutineDispatcher = TestCoroutineDispatcher()
+) : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+
+}
--- app/src/test/java/com/example/artbooktest/viewmodel/ArtViewModelTest.kt
+++ app/src/test/java/com/example/artbooktest/viewmodel/ArtViewModelTest.kt
@@ -1,10 +1,23 @@
 package com.example.artbooktest.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.artbooktest.MainCoroutineRule
+import com.example.artbooktest.Util.Status
+import com.example.artbooktest.getOrAwaitValueTest
 import com.example.artbooktest.repo.ArtRepository
 import com.example.artbooktest.repo.FakeArtRepository
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 class ArtViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule() //we don't want threads
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
     private lateinit var viewModel:ArtViewModel
 
@@ -14,5 +27,28 @@ class ArtViewModelTest {
 
         //Test doubles
         viewModel = ArtViewModel(FakeArtRepository())
+    }
+
+
+    //Test VM that contains livedata
+    @Test
+    fun `insert art without year returns error`(){
+        viewModel.makeArt("Mona Lisa", "Da Vinci", "")
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest() //need to convert livedata to regular data
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert art without name returns error`(){
+        viewModel.makeArt("", "Da Vinci", "1800")
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest() //need to convert livedata to regular data
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert art without artistName returns error`(){
+        viewModel.makeArt("Mona Lisa", "", "1800")
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest() //need to convert livedata to regular data
+        assertThat(value.status).isEqualTo(Status.ERROR)
     }
 }
